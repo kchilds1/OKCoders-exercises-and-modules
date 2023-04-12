61aa3a02840e9cc08306a9fe9b5dfc1fa6e44cba
kchilds1 <kenneth.childs20@gmail.com>
2023-04-12 11:03:40 -0500

    commit

---
+++ Module-1/Day-8/home.js
@@ -0,0 +1,5 @@
+//pulling in items to create my shopping cart (array)
+const hotCoco = document.getElementById("hot-coco");
+const cart= document.getElementById("cart");
+function 
+hotCoco.addEventListener("click", () => cart('hot-coco'));
--- Module-2/Day-2-class-exercise/index.js
+++ Module-2/Day-2-class-exercise/index.js
@@ -2,18 +2,22 @@
 let bottlesOfBeer = prompt("How many bottles of beer on the wall?");
 //adding the take on down button
 const takeDown =  document.getElementById("take-down-button");
-
+const appendP = document.getElementById("append-p")
 
 
 takeDown.addEventListener("click", () =>{
     bottlesOfBeer-= 1;
+    if (bottlesOfBeer < 1) {
+        const zero = document.createElement("p");
+        zero.textContent = "Zero bottles of beer on the wall!";
+        appendP.appendChild(zero);
+        return;
+    }
     const beerMessage = bottlesOfBeer + " bottles of beer on the wall, " + bottlesOfBeer + " bottles of beer , you take one down pass it around " + (bottlesOfBeer -1) + " bottles of beer on the wall!";
     const newP = document.createElement("p");
-    const node = document.createTextNode(beerMessage);
-    newP.appendChild(node);
-    const appendP = document.getElementById("append-p");
+    const message = document.createTextNode(beerMessage);
+    newP.appendChild(message);
     appendP.appendChild(newP);
-
     })
     
     
